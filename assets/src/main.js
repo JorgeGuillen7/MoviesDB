b7aada7d970cd0784b89ef0ddbcc54046623634e
Jorge Guillen <jguillenm1500@alumno.ipn.mx>
2022-07-04 19:51:25 -0500

    Modify API requests to axios

--- assets/src/main.js
+++ assets/src/main.js
@@ -1,13 +1,20 @@
-const API_URL = "https://api.themoviedb.org/3/";
+const API = axios.create({
+    baseURL: "https://api.themoviedb.org/3/",
+    headers: {
+        "Content-Type": "application/json;charset=utf-8",
+    },
+    params: {
+        api_key: API_KEY,
+    },
+});
 
 const getTrendingPreview = async () => {
-    const res = await fetch(`${API_URL}trending/all/week?api_key=${API_KEY}`);
-    const data = await res.json();
-
+    const { data } = await API(`trending/all/week`);
     const results = data.results;
+
     const moviesContainer = document.querySelector("#trendingPreview");
 
-    results.map((movie) => {
+    results.forEach((movie) => {
         let name;
         if (movie.media_type === "movie") {
             name = movie.title;
@@ -31,13 +38,12 @@ const getTrendingPreview = async () => {
 };
 
 const getTrendingMoviesPreview = async () => {
-    const res = await fetch(`${API_URL}trending/movie/week?api_key=${API_KEY}`);
-    const data = await res.json();
-
+    const { data } = await API(`trending/movie/week`);
     const results = data.results;
+
     const moviesContainer = document.querySelector("#trendingMoviesPreview");
 
-    results.map((movie) => {
+    results.forEach((movie) => {
         const { title, poster_path } = movie;
         const movieItem = document.createElement("div");
         movieItem.className = "movie-item";
@@ -55,13 +61,12 @@ const getTrendingMoviesPreview = async () => {
 };
 
 const getTrendingTVShowsPreview = async () => {
-    const res = await fetch(`${API_URL}trending/tv/week?api_key=${API_KEY}`);
-    const data = await res.json();
-
+    const { data } = await API(`trending/tv/week`);
     const results = data.results;
+
     const moviesContainer = document.querySelector("#trendingTVShowsPreview");
 
-    results.map((movie) => {
+    results.forEach((movie) => {
         const { name, poster_path } = movie;
         const movieItem = document.createElement("div");
         movieItem.className = "movie-item";
@@ -79,10 +84,9 @@ const getTrendingTVShowsPreview = async () => {
 };
 
 const getCategories = async () => {
-    const res = await fetch(`${API_URL}/genre/movie/list?api_key=${API_KEY}`);
-    const data = await res.json();
-
+    const { data } = await API(`genre/movie/list`);
     const results = data.genres;
+
     const categoriesContainer = document.querySelector("#moviesCategories");
 
     console.log(results);
