1cfedcd9b03499a3fa4367561eba82b7d1eea453
Jorge Guillen <jguillenm1500@alumno.ipn.mx>
2022-07-06 15:53:26 -0500

    Create node.js

---
+++ assets/src/nodes.js
@@ -0,0 +1,16 @@
+const $ = (selector) => document.querySelector(selector);
+
+const principalHeader = $("#principalHeader");
+const movieDetailsHeader = $("#movieDetailsHeader");
+const searchHeader = $("#searchHeader");
+const categoryHeader = $("#categoryHeader");
+
+const mainSection = $("#mainSection");
+
+const trendingPreview = $("#trendingPreview");
+const trendingMoviesPreview = $("#trendingMoviesPreview");
+const trendingTVShowsPreview = $("#trendingTVShowsPreview");
+const moviesCategories = $("#moviesCategories");
+
+const movieDetailsSection = $("#movieDetailsSection");
+const genericSection = $("#genericSection");
